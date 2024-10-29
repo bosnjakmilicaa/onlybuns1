8795e092e6685740359e654a71b03c069a46c823
bosnjakmilicaa <128394791+bosnjakmilicaa@users.noreply.github.com>
2024-10-29 20:39:53 +0100

    Update

--- src/main/java/com/project/onlybuns/controller/WelcomeController.java
+++ src/main/java/com/project/onlybuns/controller/WelcomeController.java
@@ -1,9 +1,11 @@
 package com.project.onlybuns.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 public class WelcomeController {
 
     @GetMapping("/")  // API ruta
