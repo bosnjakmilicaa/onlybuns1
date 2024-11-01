93c74f848fa2fd85b9e1b86e3edca6e9aaa1fed0
bosnjakmilicaa <128394791+bosnjakmilicaa@users.noreply.github.com>
2024-11-01 13:25:06 +0100

    Update users1.sql

--- src/main/resources/data/users1.sql
+++ src/main/resources/data/users1.sql
@@ -3,4 +3,19 @@ VALUES
     ('testuser2@example.com', 'password123', 'tester2', 'admin', 'Test', 'User2', '456 Test Avenue', TRUE);
 INSERT INTO users (email, password, username, user_type, first_name, last_name, address, is_active)
 VALUES
-    ('testuser3@example.com', 'password123', 'tester3', 'admin', 'Test', 'User3', '456 Test Avenue', TRUE);
+    ('testuser3@example.com', 'password123', 'tester3', 'admin', 'Test', 'User3', '457 Test Avenue', TRUE);
+INSERT INTO users (email, password, username, user_type, first_name, last_name, address, is_active)
+VALUES
+    ('testuser1@example.com', 'password123', 'tester1', 'unregistered_user', 'Test', 'User1', '459 Test Avenue', TRUE);
+INSERT INTO users (email, password, username, user_type, first_name, last_name, address, is_active)
+VALUES
+    ('testuser4@example.com', 'password123', 'tester4', 'registered_user', 'Test', 'User4', '450 Test Avenue', TRUE);
+INSERT INTO users (email, password, username, user_type, first_name, last_name, address, is_active)
+VALUES
+    ('testuser5@example.com', 'password123', 'tester5', 'registered_user', 'Test', 'User5', '451 Test Avenue', TRUE);
+INSERT INTO users (email, password, username, user_type, first_name, last_name, address, is_active)
+VALUES
+    ('testuser6@example.com', 'password123', 'tester4', 'unregistered_user', 'Test', 'User6', '450 Test Avenue', TRUE);
+INSERT INTO users (email, password, username, user_type, first_name, last_name, address, is_active)
+VALUES
+    ('testuser7@example.com', 'password123', 'tester7', 'registered_user', 'Test', 'User7', '452 Test Avenue', TRUE);
