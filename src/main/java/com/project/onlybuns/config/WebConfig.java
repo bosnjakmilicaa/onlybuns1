5431d1a51cbe40bce2f970701673ef8cee92fe50
bosnjakmilicaa <128394791+bosnjakmilicaa@users.noreply.github.com>
2024-12-20 14:28:34 +0100

    Update

--- src/main/java/com/project/onlybuns/config/WebConfig.java
+++ src/main/java/com/project/onlybuns/config/WebConfig.java
@@ -2,16 +2,16 @@ package com.project.onlybuns.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/*
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/favicon.ico")
-                .addResourceLocations("classpath:/static/favicon.ico");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCachePeriod(31556926); // Cache period in seconds (1 year)
     }
-}*/
+}
